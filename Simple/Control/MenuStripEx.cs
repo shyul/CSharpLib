c2e284fe0a492788cfdfc153ffc930dd3ee64da5
Shyu Lee <shyu.lee@gmail.com>
2016-03-19 21:49:36 -0700

    Custom ToolStrip and MenuStrip

---
+++ Simple/Control/MenuStripEx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shyu.UserControl
+{
+    public partial class MenuStripEx : System.Windows.Forms.MenuStrip
+    {
+        public MenuStripEx()
+        {
+            Renderer = new CustomToolStripRenderer();
+            DoubleBuffered = true;
+        }
+    }
+}
---
+++ Simple/Control/ToolStripEx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Shyu.UserControl
+{
+    public partial class ToolStripEx : System.Windows.Forms.ToolStrip
+    {
+        public ToolStripEx()
+        {
+            Renderer = new CustomToolStripRenderer();
+            DoubleBuffered = true;
+        }
+    }
+    public class CustomToolStripRenderer : ToolStripProfessionalRenderer
+    {
+        public CustomToolStripRenderer() { RoundedEdges = false; }
+
+        protected override void OnRenderToolStripBackground(ToolStripRenderEventArgs e)
+        {
+            e.Graphics.FillRectangle(new SolidBrush(Color.LightGray), e.AffectedBounds);
+        }
+    }
+}
