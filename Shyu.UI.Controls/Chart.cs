10101583ffedb64cdc7dd7fd66d112976c8aa7e0
Shyu Lee <shyu.lee@gmail.com>
2016-04-09 12:35:38 -0700

    Chart Form

--- Shyu.UI.Controls/Chart.cs
+++ Shyu.UI.Controls/Chart.cs
@@ -2,9 +2,11 @@
 using Shyu.UI.Drawing;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,6 +16,7 @@ namespace Shyu.UI.Controls
     public partial class Chart : UserControl
     {
         public Queue<double[]> Input = new Queue<double[]>();
+        public BackgroundWorker UpdateWorker = new BackgroundWorker();
 
         private double[] Data;
 
@@ -21,14 +24,39 @@ namespace Shyu.UI.Controls
         {
             components = new System.ComponentModel.Container();
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            UpdateWorker.WorkerReportsProgress = true;
+            UpdateWorker.WorkerSupportsCancellation = true;
+            UpdateWorker.DoWork += new DoWorkEventHandler(UpdateWorker_DoWork);
+            UpdateWorker.ProgressChanged += new ProgressChangedEventHandler(UpdateWorker_ProgressChanged);
+            UpdateWorker.RunWorkerAsync();
             Paint += new PaintEventHandler(PaintTrace);
+            SizeChanged += new EventHandler(UpdateGraphics);
         }
-
+        private void UpdateWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (Input.Count > 0) UpdateWorker.ReportProgress(1);
+                Thread.Sleep(50);
+            }
+        }
+        private void UpdateWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage != 0)
+                while (Input.Count > 0)
+                {
+                    Data = Input.Dequeue();
+                    Invalidate();
+                }
+        }
+        private void UpdateGraphics(object sender, EventArgs e)
+        {
+            this.Invalidate();      // Call PaintEventHandler for controls
+        }
         private void PaintTrace(Object sender, PaintEventArgs pe)
         {
             Graphics g = pe.Graphics;
             g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            if (Input.Count > 0) Data = Input.Dequeue();
 
             Rectangle r = new Rectangle(new Point(0, 0), new Size(Size.Width - 1, Size.Height - 1));
             g.DrawRectangle(new Pen(Color.DimGray, 1), r);
@@ -38,11 +66,11 @@ namespace Shyu.UI.Controls
 
                 List<PointF> DrawPts = new List<PointF>();
                 DataRange dr = new DataRange(Data);
-                float PointWidth = Size.Width / (Data.Length - 1);
+                float PointWidth = (float)Size.Width / ((float)Data.Length - 1.0f);
                 float Value_X = 0;
                 for (int i = 0; i < Data.Length; i++)
                 {
-                    DrawPts.Add(new PointF(Value_X, uDraw.GetPixFromYvalueF(false, Size.Height - 1, 0, dr.Minimum, dr.Maximum, Data[i])));
+                    DrawPts.Add(new PointF(Value_X, uDraw.GetPixFromYvalueF(false, Size.Height - 1, 0, dr.Maximum - 100, dr.Maximum, Data[i])));
                     Value_X += PointWidth;
                 }
                 if (DrawPts.Count > 1)
